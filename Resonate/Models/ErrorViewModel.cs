438a436be035054f3d438de089929083cb00a7e5
Dharmang Gajjar <dharmang.gajjar92@gmail.com>
2023-04-04 15:21:41 +0530

    Initial project structure setup

---
+++ Resonate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Resonate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
